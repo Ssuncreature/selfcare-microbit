2136400335848295410c2b0aaff7f8fdc4ada53f
Ssuncreature <sunniiflowo.dawn@gmail.com>
2026-05-25 14:26:56 +1200

    Basic Progress Bar

--- main.py
+++ main.py
@@ -1,16 +1,29 @@
 GoalA = 0
 GoalB = 0
-Overall = GoalA + GoalB
-if GoalA > 3:
-    GoalA = 0
-if GoalB > 2:
-    GoalB = 0
 
 def on_button_pressed_a():
     global GoalA
     GoalA = GoalA + 1
+    if GoalA > 3:
+        GoalA = 0
 input.on_button_pressed(Button.A, on_button_pressed_a)
 def on_button_pressed_b():
     global GoalB
     GoalB = GoalB + 1
+    if GoalB > 2:
+        GoalB = 0
 input.on_button_pressed(Button.B, on_button_pressed_b)
+
+def on_forever():
+    Overall = GoalA + GoalB
+    if Overall == 5:
+        led.plot(4, 4)
+    if Overall >= 4:
+        led.plot(3, 4)
+    if Overall >= 3:
+        led.plot(2, 4)
+    if Overall >= 2:
+        led.plot(1, 4)
+    if Overall >= 1:
+        led.plot(0, 4)
+basic.forever(on_forever)
